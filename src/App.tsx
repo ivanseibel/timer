8efe6cd2260c4a106f6b395765c0ca602db41b73
Ivan Seibel <me@ivanseibel.dev>
2022-12-08 20:16:06 +0000

    implement routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Timer project</h1>
-
-      <Button variant={'primary'}>Primary</Button>
-
-      <Button variant={'secondary'}>Secondary</Button>
-
-      <Button variant={'danger'}>Danger</Button>
-
-      <Button variant={'success'}>Success</Button>
-
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
