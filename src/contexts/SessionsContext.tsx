03be9b8aadf60af55eccca0fad02921bb6ed86f6
Ivan Seibel <me@ivanseibel.dev>
2022-12-11 14:32:37 +0000

    creates global context for sessions

---
+++ src/contexts/SessionsContext.tsx
@@ -0,0 +1,115 @@
+import { createContext, ReactNode, useState } from 'react'
+
+interface CreateSessionData {
+  task: string
+  minutesAmount: number
+}
+
+interface Session {
+  id: string
+  task: string
+  minutesAmount: number
+  startedAt: Date
+  interruptedAt?: Date
+  finishedAt?: Date
+}
+
+interface SessionsContextData {
+  activeSession: Session | undefined
+  activeSessionId: string | null
+  amountSecondsPassed: number
+  sessions: Session[]
+  markCurrentSessionAsFinished: () => void
+  updateAmountSecondsPassed: (seconds: number) => void
+  createNewSession: (data: CreateSessionData) => void
+  stopCountDown: () => void
+}
+
+interface SessionsContextProviderProps {
+  children: ReactNode
+}
+
+export const SessionsContext = createContext({} as SessionsContextData)
+
+export function SessionsContextProvider({
+  children,
+}: SessionsContextProviderProps) {
+  const [sessions, setSessions] = useState<Session[]>([])
+  const [activeSessionId, setActiveSessionId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const activeSession = sessions.find(
+    (session) => session.id === activeSessionId,
+  )
+
+  function markCurrentSessionAsFinished() {
+    setSessions((oldSessions) =>
+      oldSessions.map((session) => {
+        if (session.id === activeSessionId) {
+          return {
+            ...session,
+            finishedAt: new Date(),
+          }
+        }
+
+        return session
+      }),
+    )
+
+    setActiveSessionId(null)
+  }
+
+  function updateAmountSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function createNewSession(data: CreateSessionData) {
+    const newSession: Session = {
+      id: new Date().getTime().toString(),
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startedAt: new Date(),
+    }
+
+    setAmountSecondsPassed(0)
+
+    setSessions((oldSessions) => [...oldSessions, newSession])
+    setActiveSessionId(newSession.id)
+
+    // reset()
+  }
+
+  function stopCountDown() {
+    setSessions((oldSessions) =>
+      oldSessions.map((session) => {
+        if (session.id === activeSessionId) {
+          return {
+            ...session,
+            interruptedAt: new Date(),
+          }
+        }
+
+        return session
+      }),
+    )
+    setActiveSessionId(null)
+    setAmountSecondsPassed(0)
+  }
+
+  return (
+    <SessionsContext.Provider
+      value={{
+        activeSession,
+        activeSessionId,
+        amountSecondsPassed,
+        sessions,
+        markCurrentSessionAsFinished,
+        updateAmountSecondsPassed,
+        createNewSession,
+        stopCountDown,
+      }}
+    >
+      {children}
+    </SessionsContext.Provider>
+  )
+}
